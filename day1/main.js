fbb4c31ddcd08544b8d290452203d452e1ca0713
wangtyphoon <mfw9889@gmail.com>
2023-06-25 01:33:59 +0800

    day1b

--- day1/main.js
+++ day1/main.js
@@ -1,4 +1,20 @@
 const svg = d3.select('body')
                 .append('svg')
                 .attr('width',500)
-                .attr('height',300);
+                .attr('height',400);
+
+const height_male = svg.append("g")
+                        .attr('transform','translate(100,100)');
+
+height_male.append('line').attr('x1',0).attr('x2',173.5).style('stroke','black');
+height_male.append('circle').attr('cx',175).attr('cy',0).attr('r',3);
+height_male.append('text').attr('x',0).attr('y',20)
+                        .text("台灣男生平均身高173.5 cm");
+
+const height_female = svg.append("g")
+                        .attr('transform','translate(100,200)');
+
+height_female.append('line').attr('x1',0).attr('x2',173.5).style('stroke','black');
+height_female.append('circle').attr('cx',175).attr('cy',0).attr('r',3);
+height_female.append('text').attr('x',0).attr('y',20)
+                        .text("台灣女生平均身高161.5 cm");
